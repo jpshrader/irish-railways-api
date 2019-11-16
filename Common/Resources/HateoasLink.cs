9ed5cb6bc24d50ba3988c63815514fdce9b2c21a
jpshrader <46694288+jpshrader@users.noreply.github.com>
2019-11-16 18:49:57 -0500

    Splice arguments into hateoas links

--- Common/Resources/HateoasLink.cs
+++ Common/Resources/HateoasLink.cs
@@ -1,4 +1,6 @@
-﻿namespace irish_railways_api.Common.Resources {
+﻿using System.Text.RegularExpressions;
+
+namespace irish_railways_api.Common.Resources {
 	public class HateoasLink {
 		public const string GET_SELF = "self";
 		public const string GET_SELF_DETAILED = "self_detailed";
@@ -15,8 +17,21 @@
 			Method = method.ToString();
 		}
 
-		public static HateoasLink BuildGetLink(string href, string rel) {
-			return new HateoasLink(href, rel, HateoasMethod.GET);
+		public static HateoasLink BuildGetLink(string href, string rel, params string[] routeArgs) {
+			return new HateoasLink(SpliceArgsIntoRoute(href, routeArgs), rel, HateoasMethod.GET);
+		}
+
+		public static string SpliceArgsIntoRoute(string href, params string[] routeArgs) {
+			if (routeArgs.Length == 0)
+				return href;
+
+			var splicedHref = "";
+			foreach (var arg in routeArgs) {
+				var isolatedArgString = href.Substring(0, href.IndexOf('}') + 1);
+				splicedHref = Regex.Replace(isolatedArgString, "{.*}", arg);
+			}
+
+			return splicedHref;
 		}
 	}
 }
